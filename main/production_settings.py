08987bab906efe62594190ff91f4704ea2d9dc67
blunk21 <g.rockandroller@gmail.com>
2023-11-03 12:10:36 +0100

    apps copied from local settings (#2)
    Co-authored-by: sarADC21 <gergely.bereczki@toolsense.io>

--- main/production_settings.py
+++ main/production_settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "configs.apps.Configs",
+    "module_messages.apps.ModuleMessages",
     "rest_framework",
     "corsheaders",
 ]
