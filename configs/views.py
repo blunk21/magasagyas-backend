ed2027e9d9514dff7abea3169ceae3b6ddae99c8
sarADC21 <gergely.bereczki@toolsense.io>
2023-11-25 15:25:50 +0100

    asd

--- configs/views.py
+++ configs/views.py
@@ -7,37 +7,37 @@ from .serializers import ConfigSerializer
 from .forms import ConfigCreationForm
 
 import json
+
 # Create your views here.
 
-class ConfigsView(
-    APIView
-):
-    def get(self,request,id):
+
+class ConfigsView(APIView):
+    def get(self, request, id):
         try:
             latest_config = Config.objects.latest("id")
-            if latest_config.id>id:
+            if latest_config.id > id:
                 return Response(latest_config.config)
-            elif latest_config.id==id:
+            elif latest_config.id == id:
                 return Response(status=200)
             else:
                 return Response(status=404)
         except Config.DoesNotExist:
-  
             return Response(status=404)
-        
+
+
 def create_new_config(request):
     if request.method == "POST":
         form = ConfigCreationForm(request.POST)
         if form.is_valid():
             config_data = {
-                "alarm1" : form.cleaned_data["alarm1"].isoformat(),
-                "alarm2" : form.cleaned_data["alarm2"].isoformat(),
-                "duration1" : form.cleaned_data["duration1"],
-                "duration" : form.cleaned_data["duration2"],
-                "wakeup_freq" : form.cleaned_data["wakeup_freq"],
+                "alarm1": form.cleaned_data["alarm1"].isoformat(),
+                "alarm2": form.cleaned_data["alarm2"].isoformat(),
+                "duration1": form.cleaned_data["duration1"],
+                "duration": form.cleaned_data["duration2"],
+                "wakeup_freq": form.cleaned_data["wakeup_freq"],
             }
             Config.objects.create(config=json.dumps(config_data))
             return redirect("create_new_config")
     else:
         form = ConfigCreationForm()
-    return render(request, "config_change.html",{'form': form})
+    return render(request, "config_change.html", {"form": form})
--- module_messages/serializers.py
+++ module_messages/serializers.py
@@ -7,10 +7,14 @@ import datetime
 
 class ModuleMessageSerializer(serializers.ModelSerializer):
     def create(self,validated_data):
-        ts = self.initial_data.get("ts")
+        ts= self.initial_data.get("ts")
+        if ts is None:
+            raise serializers.ValidationError({"error": "Module message is missing timestamp"})
         timestamp = datetime.datetime.fromtimestamp(ts)
         readable_time = f"{timestamp.year}-{timestamp.month}-{timestamp.day} {timestamp.hour}:{timestamp.minute}:{timestamp.second}"
-        return ModuleMessages.objects.create(data=validated_data,timestamp=timestamp)
+
+        validated_data = self.initial_data
+        return ModuleMessages.objects.create(data=validated_data,timestamp=readable_time)
     
     class Meta:
         model=ModuleMessages
